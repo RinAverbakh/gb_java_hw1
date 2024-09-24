b2eea492fcb0d747e9fac265b8ca030bb8d2cfc1
Rin <arina.averbakh.st@gmail.com>
2024-09-24 11:23:05 +0300

    Написан метод для решения задания 1

--- src/App.java
+++ src/App.java
@@ -1,7 +1,26 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
         
-        System.out.println("Hello, World!");
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Введите число: ");
+        int n = scan.nextInt();
+        int fact = factorial(n);
+        System.out.println("Факториал числа " + n + " равен " + fact);
 
+    }
+
+    public static int factorial(int n) {
+
+        int result = 1 ;
+        if(n < 0) result = -1;
+        else if (n > 1){
+            for (int i = 2; i <= n; i++) {
+                result *= i;
+            }
+        }
+
+        return(result);
     }
 }

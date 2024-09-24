e59fb0d47fbc2fcd1d08ca9cdf5b25d1a1b97e2c
Rin <arina.averbakh.st@gmail.com>
2024-09-24 12:24:46 +0300

    Написаны методы для решения задания 4

--- src/App.java
+++ src/App.java
@@ -4,15 +4,23 @@ public class App {
     public static void main(String[] args) throws Exception {
         
         Scanner scan = new Scanner(System.in);
-        System.out.print("Введите число: ");
-        int n = scan.nextInt();
+        // System.out.print("Введите число: ");
+        // int n = scan.nextInt();
         // int fact = factorial(n);
         // System.out.println("Факториал числа " + n + " равен " + fact);
 
         // printEvenNums();
 
-        int summ = sumDigits(n);
-        System.out.println("Сумма цифр числа " + n + " равна " + summ);
+        // int summ = sumDigits(n);
+        // System.out.println("Сумма цифр числа " + n + " равна " + summ);
+
+        System.out.print("Введите первое число: ");
+        int а = scan.nextInt();
+        System.out.print("Введите второе число: ");
+        int b = scan.nextInt();
+        System.out.print("Введите третье число: ");
+        int c = scan.nextInt();
+        System.out.println("Большим является число " + findMaxOfThree(а, b, c));
 
     }
 
@@ -46,5 +54,16 @@ public class App {
         }
 
         return(result);
+    }
+
+    public static int findMaxOfTwo (int a, int b) {
+
+        if (a >= b) return(a);
+        else return(b);
+
+    }
+
+    public static int findMaxOfThree(int a, int b, int c) {
+        return(findMaxOfTwo(a, findMaxOfTwo(c, b)));
     }
 }
